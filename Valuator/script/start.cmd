5f6d264aa01c9506df7545b6b9db18e256a7d798
Alexei Moskvin <amoskvin@uni-systems.ru>
2024-10-19 19:00:12 +0300

    Добавил 6 лабораторную

--- Valuator/script/start.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-start cmd /c "cd ..\nats && nats-server"
-start cmd /c "cd ..\RankCalculator && dotnet run"
-start cmd /c "cd ..\EventLogger && dotnet run"
-start cmd /c "cd ..\EventLogger && dotnet run"
-start cmd /c "cd ..\Valuator && dotnet run --urls "http://0.0.0.0:5001""
-start cmd /c "cd ..\Valuator && dotnet run --urls "http://0.0.0.0:5002""
-start cmd /c "cd ..\nginx && nginx"
--- Valuator/script/stop.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-start cmd /c "taskkill /f /im valuator.exe"
-start cmd /c "taskkill /f /im nginx.exe"
-start cmd /c "taskkill /f /im RankCalculator.exe"
-start cmd /c "taskkill /f /im nats-server.exe"
-start cmd /c "taskkill /f /im EventLogger.exe"
